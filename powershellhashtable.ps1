24c2508742503e700afac07a13def80fb94bec23
Akanksha980 <78799253+Akanksha980@users.noreply.github.com>
2021-02-10 09:01:31 +0530

    Add files via upload

---
+++ powershellhashtable.ps1
@@ -0,0 +1,20 @@
+﻿#commenting-statement
+#practice: Powershell Hash table
+#Author: Akanksha
+#platform- powerhsell ise -powered by windows
+#------------script starts here----------
+
+$Employee=@{Eid=101; Ename="Aman Mittal"; Designation="Team Lead"; Salary=100000; city="New Delhi"}
+echo $Employee
+echo "----<>------"
+echo $Employee.Keys
+echo "----<>------"
+echo $Employee.Values
+echo "----<>------"
+echo $Employee.Count
+echo "----<>------"
+echo $Employee.IsReadOnly
+echo "----<>------"
+echo $Employee.IsFixedSize
+echo "----<>------"
+echo $Employee.IsSynchronized
