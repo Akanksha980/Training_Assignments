1c71a89b3c2f17f06c13793116bbb9f1e89ea162
Akanksha980 <78799253+Akanksha980@users.noreply.github.com>
2021-02-10 09:00:15 +0530

    Add files via upload

---
+++ powershellassignmentandarithmeticoperator.ps1
@@ -0,0 +1,70 @@
+﻿#commenting-statement
+#practice: Powershell arithmetic and assignment operators
+#Author: Akanksha
+#platform- powerhsell ise -powered by windows
+#------------script starts here----------
+$var1=100
+$var2=20
+echo "Addition operator"
+$res1=$var1+$var2
+write-host $res1
+echo $res1
+
+echo "------------------------"
+echo "Subtraction operator"
+$sub=$var1-$var2
+echo $sub
+
+echo "------------------------"
+echo "Multiplication Operator"
+$mul=$var1*$var2
+echo $mul
+
+echo "-----------------------"
+echo " / operator"
+$quo=$var1/$var2
+echo $quo
+
+echo "-----------------------"
+echo " % operator"
+$mod=$var1%$var2
+echo $mod
+
+echo "-----------------------"
+echo " Increment operator"
+$num=98
+$R=$num++
+echo $R
+echo $num
+echo "pre-increment"
+$d=10
+$F=++$d
+echo $F
+echo $d
+echo "-----------------------"
+echo "Decrement operator"
+$q=65
+echo $q
+$s=$q--
+echo $q
+echo $s
+echo "pre-decrement"
+$x=20
+echo $x
+$cc= --$x
+echo $xecho $cc
+
+$wordd="Power"
+echo $wordd
+$wordd+="-shell"
+echo $wordd
+
+$ab=20
+echo $ab
+$ab+=10
+echo $ab
+$ab-=5
+echo $ab
+
+
+
