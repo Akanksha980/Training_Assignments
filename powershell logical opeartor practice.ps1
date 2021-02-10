c8abeced3e18805fa6c1d9e604b17a07b785a770
Akanksha980 <78799253+Akanksha980@users.noreply.github.com>
2021-02-10 09:04:40 +0530

    Add files via upload

---
+++ powershell logical opeartor practice.ps1
@@ -0,0 +1,22 @@
+﻿#commenting-statement
+#practice: Powershell logical operators
+#Author: Akanksha
+#platform- powerhsell ise -powered by windows
+#------------script starts here----------
+$a=10
+$b=20
+Echo "For OR operator"
+($a-lt$b)-or($a-gt$b)
+
+Echo "For AND operator"
+($a-lt$b)-and($a-gt$b)
+
+Echo "For NOT operator"
+-not($a-lt$b)
+
+Echo "for negation ! operator"
+!($a-eq$b)
+
+Echo "For XOR operqtor"
+($a-eq$b)-or($a-lt$b)
+
